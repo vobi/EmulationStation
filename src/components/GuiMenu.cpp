49b2cbdb33b80b0702da52dd3c545333075790e1
vobi <aeux.d@gmx.de>
2012-10-27 19:28:06 +0200

    add git backup functionality
    In order to get it working you have to set up the git repository properly. Here is what i did:
    
    append client's .ssh/id_rsa.pub to server's .ssh/authorized_keys
    
    on server:
    mkdir savegames.git
    cd savesgames.git
    git --bare init
    
    on client
    git clone ssh://user@serverurl:sshport/~/savegames.git
    mv savegames/.git RetroPie/roms
    rmdir savegames
    cd RetroPie/roms
    git add */*.srm
    git commit -m "initial commit"
    git push origin master
    
    Note that only *.srm files of subdirectories of the roms folder (doesn't need to be named that way) are under control of git. I don't know if there are other extensions for the savegames, I only played gbc and snes which had these extensions.

--- src/components/GuiMenu.cpp
+++ src/components/GuiMenu.cpp
@@ -51,6 +51,14 @@ void GuiMenu::populateList()
 
 	mList->addObject("Restart", "sudo shutdown -r now", 0x0000FFFF);
 	mList->addObject("Shutdown", "sudo shutdown -h now", 0x0000FFFF);
+	
+	std::string romsFolderPath = SystemData::sSystemVector.at(0)
+					->getRootFolder()->getPath() + "/../";
+	std::string git = "git --git-dir "+romsFolderPath+".git";
+	std::string command = git+" --work-tree "+romsFolderPath+" add "+
+		romsFolderPath+"*/*.srm;"+git+" commit -m 'savegames';" + git +" push";
+	mList->addObject("git backup", command, 0x0000FFFF);
+	mList->addObject("git pull", git + " pull", 0x0000FFFF);
 }
 
 void GuiMenu::onRender()
